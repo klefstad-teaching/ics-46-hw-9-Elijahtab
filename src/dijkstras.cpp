41d17a4d78ad5d843a03cb8dab64059b98375ebb
Elijah <elijah.tabachnik@gmail.com>
2025-03-15 23:04:04 -0700

    s

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,63 +1,76 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <limits>
-#include <algorithm>
+#include "dijkstras.h"
 
-using namespace std;
+Graph::Graph(int n) : numVertices(n) {
+    adjacencyList.resize(n);
+    distance.resize(n, INF);
+    previous.resize(n, -1);
+    visited.resize(n, false);
+}
 
-constexpr int INF = numeric_limits<int>::max();
+Graph::Graph() : numVertices(0) {}
 
-struct Node {
-    int vertex;
-    int distance;
-    bool operator>(const Node& other) const {
-        return distance > other.distance; 
-    }
-};
-
-struct Edge {
-    int src, dst, weight;
-    Edge(int s, int d, int w) : src(s), dst(d), weight(w) {}
-};
-
-struct Graph {
-    int numVertices;
-    vector<vector<Edge>> adjacencyList;
-    vector<int> dist;
-    vector<int> prev;
-    vector<bool> visited;
-
-    Graph(int n) : numVertices(n) {
-        adjacencyList.resize(n);
-        dist.resize(n, INF);
-        prev.resize(n, -1);
-        visited.resize(n, false);
-    }
-
-    void addEdge(int u, int v, int w, bool undirected = true) {
-        adjacencyList[u].push_back(Edge(u, v, w));
-        if (undirected) {
-            adjacencyList[v].push_back(Edge(v, u, w));
+void Graph::addEdge(int u, int v, int weight, bool isUndirected) {
+    bool found = false;
+    for (auto& e : adjacencyList[u]) {
+        if (e.dst == v) {
+            found = true;
+            break;
         }
     }
-};
+    if (!found) {
+        adjacencyList[u].push_back(Edge(u, v, weight));
+    }
+    if (isUndirected) {
+        found = false;
+        for (auto& e : adjacencyList[v]) {
+            if (e.dst == u) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            adjacencyList[v].push_back(Edge(v, u, weight));
+        }
+    }
+}
+
+istream& operator>>(istream& in, Graph& G) {
+    if (!(in >> G.numVertices)) {
+        throw runtime_error("Unable to read numVertices");
+    }
+    G.adjacencyList.resize(G.numVertices);
+    G.distance.resize(G.numVertices, INF);
+    G.previous.resize(G.numVertices, -1);
+    G.visited.resize(G.numVertices, false);
+
+    for (Edge e; in >> e;) {
+        G.addEdge(e.src, e.dst, e.weight, true);
+    }
+    return in;
+}
+
+void file_to_graph(const string& filename, Graph& G) {
+    ifstream file(filename);
+    if (!file) {
+        throw runtime_error("Can't open input file: " + filename);
+    }
+    file >> G;
+    file.close();
+}
 
 vector<int> dijkstra_shortest_path(Graph& G, int source, vector<int>& previous) {
     int n = G.numVertices;
-    G.dist.assign(n, INF);
+    G.distance.assign(n, INF);
     G.visited.assign(n, false);
     previous.assign(n, -1);
 
     priority_queue<Node, vector<Node>, greater<Node>> pq;
-
-    G.dist[source] = 0;
+    G.distance[source] = 0;
     pq.push({source, 0});
 
     while (!pq.empty()) {
         Node current = pq.top();
         pq.pop();
-
         int u = current.vertex;
         if (G.visited[u]) continue;
         G.visited[u] = true;
@@ -65,14 +78,14 @@ vector<int> dijkstra_shortest_path(Graph& G, int source, vector<int>& previous) 
         for (const Edge& edge : G.adjacencyList[u]) {
             int v = edge.dst;
             int w = edge.weight;
-            if (!G.visited[v] && G.dist[u] + w < G.dist[v]) {
-                G.dist[v] = G.dist[u] + w;
+            if (!G.visited[v] && G.distance[u] + w < G.distance[v]) {
+                G.distance[v] = G.distance[u] + w;
                 previous[v] = u;
-                pq.push({v, G.dist[v]});
+                pq.push({v, G.distance[v]});
             }
         }
     }
-    return G.dist;
+    return G.distance;
 }
 
 vector<int> extract_shortest_path(const vector<int>& dist, const vector<int>& prev, int destination) {
@@ -93,6 +106,7 @@ void print_path(const vector<int>& path, int total) {
         return;
     }
     for (size_t i = 0; i < path.size(); i++) {
+
         cout << path[i] << " ";
     }
     cout << "\nTotal cost is " << total << "\n";
