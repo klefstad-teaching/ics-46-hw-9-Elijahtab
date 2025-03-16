8f17f75ba345ddedeb8084fd4255d46feb42c0ad
Elijah <elijah.tabachnik@gmail.com>
2025-03-15 21:52:14 -0700

    s

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -6,12 +6,14 @@
 #include <climits>
 
 using namespace std;
-
 vector<int> dijkstra_shortest_path(Graph& G, int source, vector<int>& previous) {
-    int n = G.adjacencyList.size();
-    G.distance[source] = 0;
+    int n = G.numVertices;
+    G.distance.assign(n, INF); 
+    G.visited.assign(n, false); 
+    previous.assign(n, -1);     
 
     priority_queue<Node, vector<Node>, greater<Node>> pq;
+    G.distance[source] = 0;
     pq.push({source, 0});
 
     while (!pq.empty()) {
@@ -22,9 +24,9 @@ vector<int> dijkstra_shortest_path(Graph& G, int source, vector<int>& previous) 
         if (G.visited[u]) continue;
         G.visited[u] = true;
 
-        for (const Node& neighbor : G.adjacencyList[u]) {
-            int v = neighbor.vertex;
-            int weight = neighbor.weight;
+        for (const Edge& edge : G.adjacencyList[u]) { // Now correctly using Edge
+            int v = edge.dst;  // Get destination vertex
+            int weight = edge.weight;
 
             if (!G.visited[v] && G.distance[u] + weight < G.distance[v]) {
                 G.distance[v] = G.distance[u] + weight;
@@ -36,6 +38,7 @@ vector<int> dijkstra_shortest_path(Graph& G, int source, vector<int>& previous) 
 
     return G.distance;
 }
+
 
 
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
